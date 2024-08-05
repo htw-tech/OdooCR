d82d596154b9fa210ccfbcdbb9a04e8873c89ba2
mrouissi <mh.rouissi@gmail.com>
2024-08-05 21:59:31 +0200

    Doxygen Documentation of MainActivity.kt

--- app/src/main/java/com/minago/odoocr/MainActivity.kt
+++ app/src/main/java/com/minago/odoocr/MainActivity.kt
@@ -5,17 +5,20 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+/*! \class MainActivity
+    \brief Main activity of the application which serves as the entry point.
+ */
 class MainActivity : AppCompatActivity() {
+
+    /*! \brief Called when the activity is starting.
+        \param savedInstanceState If the activity is being re-initialized after previously being shut down then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle).
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.btnEnterInvoice).setOnClickListener {
             startActivity(Intent(this, EnterInvoiceActivity::class.java))
-        }
-
-        findViewById<Button>(R.id.btnScannedInvoices).setOnClickListener {
-            // We'll implement this later
         }
     }
 }
